ba0013bbc20e111a7e587c00286567f082733602
sach <sachinsomia@gmail.com>
2019-04-13 23:19:21 +0100

    add /sigin and /register POST end points

--- server.js
+++ server.js
@@ -2,10 +2,53 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const database = {
+	users : [
+	{
+		id: 123,
+		name: 'John',
+		email: 'john@gmail.com',
+		password: 'cookies',
+		entries: 0,
+		joined: new Date()
+	},
+	{
+		id: 124,
+		name: 'Sally',
+		email: 'Sally@gmail.com',
+		password: 'bananas',
+		entries: 0,
+		joined: new Date()
+	}
+	]
+}
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-	res.send('Hello');
+	res.send('this is working');
+})
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password) {
+			res.json('success');
+	} else {
+		res.json('error logging in!!');
+	}
+})
+
+app.post('/register', (req, res) => {
+	const {email, name, password } = req.body
+	database.users.push({
+		id: 125,
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length -1])
 })
 
 app.listen(3000, () => console.log('The server is listening...'));
