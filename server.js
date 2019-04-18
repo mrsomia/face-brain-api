1ca3b91b86cd1a559c633fb92ed8a598bb5b4cfa
Sachin Somia <sachinsomia@Sachins-MacBook-Pro.local>
2019-04-18 17:30:16 +0100

    refactor /image endpoint to work with psql

--- server.js
+++ server.js
@@ -58,18 +58,14 @@ app.get('/profile/:id', (req, res) => {
 });
 
 app.put('/image', (req, res) => {
-	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user =>{
-		if (user.id === Number(id)) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
+	// user id in through req body json => user.entries out also in json
+	const {id} = req.body;
+	db('users')
+		.where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => res.json(entries[0])) // here you could deal with no entry count being returned
+		.catch(err => res.status(400).json('Error updating entry count'));
 })
 
 app.listen(3000, () => console.log('The server is listening on port 3000...'));
