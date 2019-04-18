199ec4c1bc976d3656f8ecf867c204dc8aacd30d
Sachin Somia <sachinsomia@Sachins-MacBook-Pro.local>
2019-04-18 19:03:48 +0100

    alter /register endpoint to hash password & store in login and user databases

--- server.js
+++ server.js
@@ -33,15 +33,28 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-	const {email, name, password } = req.body
-	db('users')
-		.returning('*')
-		.insert({
-			name: name,
-			email: email,
-			joined: new Date()})
-			.then(user => res.json(user[0]))
-			.catch(err => res.status(400).json('Could not register'));
+	// requires email name and password and outputs the registed user object
+	const {email, name, password } = req.body;
+	const hash = bcrypt.hashSync(password, 10);
+	db.transaction(trx => {
+		trx.insert({email, hash})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+					.returning('*')
+					.insert({
+						name: name,
+						email: loginEmail[0],
+						joined: new Date()
+					})
+					.then(user => res.json(user[0]))
+					.catch(err => res.status(400).json('Could not register'));
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('Error registering'));
 })
 
 app.get('/profile/:id', (req, res) => {
