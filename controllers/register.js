aec0514ea8a8eadb09e81d43f10675da7862a86b
Sachin Somia <sachinsomia@Sachins-MacBook-Pro.local>
2019-04-24 14:10:52 +0100

    Added simple validation for registration and signin

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
 	// requires email name and password and outputs the registed user object
 	const {email, name, password } = req.body;
+	if (!email || !name || !password) {
+		return  res.status(400).json('Incorrect Form Submission');
+	}
 	const hash = bcrypt.hashSync(password, 10);
 	db.transaction(trx => {
 		trx.insert({email, hash})
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,8 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
 	const {email, password} = req.body;
+	if (!email || !password) {
+		return  res.status(400).json('Incorrect Form Submission');
+	}
 	db.select('*').from('login').where({email})
 		.then(data => {
 			const isValid = bcrypt.compareSync(password, data[0].hash);
